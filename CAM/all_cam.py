fbf611c2b7338b22e82ae5ccb520c4a39ba8bed0
瓦下听风 <haihaoxxx@163.com>
2022-09-10 13:20:44 +0800

    新示例

--- CAM/all_cam.py
+++ CAM/all_cam.py
@@ -33,7 +33,7 @@ input_path  = "./Images"
 use_cuda = False
 
 if __name__ == '__main__':
-    for method in [i for i in methods.keys()][7:]:
+    for method in [i for i in methods.keys()][6:]:
         output_path = f'./Results/{method}'
         print(f'{method}测试')
         if not os.path.isdir(output_path):
--- run_all.bat
+++ run_all.bat
@@ -1,11 +1,13 @@
 %Install dependency library%
 pip install -r ./requirements.txt
-python .\CAM\all_cam.py
-python .\CAM\cam.py
-python .\IG\IGOS_main.py
-python .\LRP\LRP_densenet_main.py
-python .\LRP\LRP_resnet_main.py
-python .\CAM\Relevance-CAM.py --models vgg19 --target_layer 52
-python .\CAM\Relevance-CAM.py --models resnet50 --target_layer layer2
-python .\GNL\main.py -md guided_integrated_gradients -mn resnet50 -si 1 -ie 1 -ei 5 -gn 0 -da .\Images\
-python .\GNL\main.py -md guided_integrated_gradients -mn vgg19 -si 1 -ie 1 -ei 5 -gn 0 -da .\Images\
+python ./CAM/all_cam.py
+python ./CAM/cam.py
+python ./IG/IGOS_main.py
+python ./LRP/LRP_densenet_main.py
+python ./LRP/LRP_resnet_main.py
+python ./CAM/Relevance-CAM.py --models vgg19 --target_layer 52
+python ./CAM/Relevance-CAM.py --models resnet50 --target_layer layer2
+python ./SmoothGraad/SmoothGrad_resnet.py
+python ./SmoothGraad/SmoothGrad_vgg.py
+python ./GNL/main.py -md guided_integrated_gradients -mn resnet50 -si 1 -ie 1 -ei 5 -gn 0 -da ./Images/
+python ./GNL/main.py -md guided_integrated_gradients -mn vgg19 -si 1 -ie 1 -ei 5 -gn 0 -da ./Images/
